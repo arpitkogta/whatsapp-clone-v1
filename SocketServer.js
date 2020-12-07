dc74748d1764a3f3e01b872b6f172efca9374b32
Arpit Kogta <54179887+arpitkogta@users.noreply.github.com>
2020-12-07 09:31:44 +0530

    Folders uploaded in zipped format

---
+++ SocketServer.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+
+var server = app.listen('8000', () => {
+    console.log('listening on port 8000');
+});
+
+var io = socket(server);
+
+io.on('connection', function (socket) {
+    console.log('made socket connection');
+    socket.on('message', function (data) {
+            io.sockets.emit('typing', true);
+            setTimeout(()=>{
+              io.sockets.emit('chat', generateMessage());
+            }, Math.random()*7000);
+    });
+})
+
+function generateMessage() {
+    var text = "",
+    possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    length = Math.round((Math.random()*10)) || 3;
+
+    for (var i = 0; i < length; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
